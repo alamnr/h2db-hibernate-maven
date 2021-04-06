28d16f0325d3703d7318275d99b4050b39336517
alamnr <alamnr@gmail.com>
2021-04-06 10:11:46 +0600

    retrieving objects using hibernate get an load api and returned exception compring

--- src/test/java/com/guddy/h2/hibernate/AppTest.java
+++ src/test/java/com/guddy/h2/hibernate/AppTest.java
@@ -20,7 +20,11 @@ import java.util.Map;
 import javax.transaction.SystemException;
 
 import org.hibernate.HibernateException;
+import org.hibernate.ObjectNotFoundException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.infiniteskills.data.HibernateUtil;
@@ -49,9 +53,12 @@ public class AppTest
         assertTrue( true );
     }
     
+    
+    private static final SessionFactory  sessionFactory = HibernateUtil.getSessionFactory();
+    
     @Test
     public void openCloseSession() {
-    	Session session =HibernateUtil.getSessionFactory().openSession();
+    	Session session =sessionFactory.openSession();
     	session.beginTransaction();
     	assertEquals(true,session.isOpen() );
     	
@@ -66,35 +73,35 @@ public class AppTest
 		 
 		Bank bank = null;
 		Address address;
-		Session session = HibernateUtil.getSessionFactory().openSession();
+		address =  new Address("addressline1", "addressline2", "California", "Ohio", "1234");
+		 Map<String,String> mapValueType = new HashMap<>();
+		  mapValueType.put("Manager", "Jagan"); mapValueType.put("Neta", "Samvu");
+		 
+		bank = new Bank(null, "Shaoa Bank", address, false, new Date(), "chodna", new Date(), "madna",
+				mapValueType);	
+		
+		Session session = sessionFactory.openSession();
+		
+		
 		try {
 			session.beginTransaction();
-			address =  new Address("addressline1", "addressline2", "California", "Ohio", "1234");
 			/*
 			 * List<String> collect = new ArrayList<>(); collect.add("A");
 			 * collect.add("Computer"); collect.add("Portal"); collect.add("for");
-			 * collect.add("Geeks");
-			 */
+			 * collect.add("Geeks");			 */			
 			
-			  Map<String,String> mapValueType = new HashMap<>();
-			  mapValueType.put("Manager", "Jagan"); mapValueType.put("Neta", "Samvu");
-			 
 			
-			bank = new Bank(null, "Shaoa Bank", address, false, new Date(), "chodna", new Date(), "madna",
-					mapValueType);
 			session.save(bank);
-			session.getTransaction().commit();
 			
+			session.getTransaction().commit();		
 			
-		
-			
-		}catch (Exception e) {
+		} catch (Exception e) {
 			session.beginTransaction().rollback();
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		} finally {
 			session.close();
-			//HibernateUtil.getSessionFactory().close();
+			
 		}	
 		
 		assertNotNull(bank.getBankId());
@@ -104,25 +111,28 @@ public class AppTest
     public void  testUserPersistence()
     {
     	
-    	Session session = HibernateUtil.getSessionFactory().openSession();
+    	Session session = sessionFactory.openSession();
     	User user;
     	Address address;
     	Credential credential;
+    	
+    	address = new Address("addressline1", "addressline2", "California", "Ohio", "1234");
+		List<Address> addresses = new ArrayList<>();
+		Address address1 =new Address("addressline3", "addressline4", "Utah", "UT", "6789");
+		addresses.add(address1);
+		addresses.add(address);
+		credential = new Credential();
+		credential.setUserName("chodna");
+		credential.setPassword("madna");
+		user = createUser("Ganesh", "Thappa", "ganesh@gmail.com");
+		credential.setUser(user);
+           	
 		try {
-			session.beginTransaction();
-			address = new Address("addressline1", "addressline2", "California", "Ohio", "1234");
-			List<Address> addresses = new ArrayList<>();
-			Address address1 =new Address("addressline3", "addressline4", "Utah", "UT", "6789");
-			addresses.add(address1);
-			addresses.add(address);
-			credential = new Credential();
-			credential.setUserName("chodna");
-			credential.setPassword("madna");
-			user = createUser("Ganesh", "Thappa", "ganesh@gmail.com");
-			credential.setUser(user);
+			session.beginTransaction();	
 			
 			//session.save(user);
 			session.save(credential);
+						   	
 			session.getTransaction().commit();
 			
 			/*
@@ -145,7 +155,7 @@ public class AppTest
 			throw new RuntimeException(e);
 		} finally {
 			session.close();
-			//HibernateUtil.getSessionFactory().close();
+			
 		}
     	
     }
@@ -160,7 +170,7 @@ public class AppTest
 
     @Test
     public void testOwningEntityCascadeOperation() {
-    	Session session = null; 
+    	Session session= sessionFactory.openSession();
     	User user = createUser("Modan","Chohan","modan@gmail.com");
     	Credential credential = new Credential();
     	
@@ -168,12 +178,19 @@ public class AppTest
     	credential.setUserName("John");
     	
     	user.setCredential(credential);
+    	
+    	System.out.println(session.contains(credential));
+    	System.out.println(session.contains(user));
     	//credential.setUser(user);
     	Long credId;
     	try {
-    		session= HibernateUtil.getSessionFactory().openSession();
+    		
 			session.beginTransaction();
 			session.save(user);
+			
+
+	    	System.out.println(session.contains(credential));
+	    	System.out.println(session.contains(user));
 			session.getTransaction().commit();
 			
 			assertNotNull(user.getCredential().getCredentialId());		
@@ -186,22 +203,22 @@ public class AppTest
 			session.getTransaction().rollback();
 			throw new  RuntimeException(e);
 		} finally {
-			session.close();
+			session.close();			
 		}
     	
     	try {
-			session = HibernateUtil.getSessionFactory().openSession();
+			session = sessionFactory.openSession();
 			
 			Credential  credential2 = session.get(Credential.class, credId);
 			System.out.println(credential2.getUser().getEmailAddress());
 		} catch (HibernateException e) {
 			throw new RuntimeException(e);
 		} finally {
-			session.close();
+			session.close();			
 		}
     	
     	try {
-    		session = HibernateUtil.getSessionFactory().openSession();
+    		session = sessionFactory.openSession();
 			session.beginTransaction();
 			session.remove(user);
 			session.getTransaction().commit();			
@@ -230,36 +247,7 @@ public class AppTest
     	
     	return user;
     }
-	@Test
-    public void persistAccountType() throws IllegalStateException, SystemException {
-    	
-		/*
-		 * AccountType accountType1 = new AccountType(null, "muksed", new Date(), new
-		 * Date(), "vuksed"); AccountType accountType2 = new AccountType(null, "chudna",
-		 * new Date(), new Date(), "madna"); List<AccountType> accList = new
-		 * ArrayList<>();
-		 * 
-		 * try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-		 * session.beginTransaction();
-		 * 
-		 * session.save(accountType1); session.save(accountType2);
-		 * 
-		 * session.getTransaction().commit();
-		 * 
-		 * } catch (Exception e) { throw e; }
-		 * 
-		 * System.out.println(accList.size());
-		 * 
-		 * try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-		 * 
-		 * accList = session.createQuery("from AccountType",AccountType.class).list();
-		 * 
-		 * } catch (Exception e) { throw e; }
-		 * 
-		 * System.out.println(accList.size()); assertEquals(accList.size(),2);
-		 */
-    	
-    }
+	
 	
 	@Test
 	public void persistAcountAndTransaction() {
@@ -269,7 +257,7 @@ public class AppTest
 		Account account = createNewAccount(accountName);
 		
 		try {
-			session = HibernateUtil.getSessionFactory().openSession();
+			session = sessionFactory.openSession();
 			session.beginTransaction();
 			account.getTransactions().add(createNewBeltPurchase(account));
 			account.getTransactions().add(createNewShoePurchase(account));
@@ -287,6 +275,7 @@ public class AppTest
 			throw new RuntimeException(e);
 		} finally {
 			session.close();
+			
 		}
 		
 		
@@ -295,7 +284,7 @@ public class AppTest
 	@Test
 	public void testAccountAndUserPersistence() {
 		
-		Session session = null;
+		Session session = sessionFactory.openSession();
 		Account account1 = createNewAccount("Current Account");
 		Account account2 = createNewAccount("fixed Deposit Account");
 		User user1 = createUser("Modan","Chohan","modan@gmail.com");
@@ -317,7 +306,7 @@ public class AppTest
 		
 		
 		try {
-			session = HibernateUtil.getSessionFactory().openSession();
+			
 			session.beginTransaction();
 			//session.save(account1);
 			//session.save(account2);
@@ -355,15 +344,14 @@ public class AppTest
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		} finally {
-			if(session!=null)
-				session.close();
+			session.close();
 		}
 		
 	}
 	
 	@Test
 	public void testBudgetPersistentJoinTableOperation() {
-		Session session = null;
+		Session session = sessionFactory.openSession();
 		Account account = createNewAccount("Savings Account");
 		Budget budget = new Budget();
 		budget.setGoalAmount(new BigDecimal(1000));
@@ -373,7 +361,6 @@ public class AppTest
 		budget.getTransactions().add(createNewShoePurchase(account));
 		
 		try {
-			session = HibernateUtil.getSessionFactory().openSession();
 			session.beginTransaction();
 			
 			session.save(budget);
@@ -385,8 +372,74 @@ public class AppTest
 			session.getTransaction().commit();
 			throw new RuntimeException(e);
 		} finally {
-			if(session != null)
-				session.close();
+			session.close();
+		}
+	}
+	
+	@Test(expected  = ObjectNotFoundException.class)
+	public void testLoadApiError() {
+		Session session = sessionFactory.openSession();
+		
+		try {			
+					
+			Bank bank1 = session.load(Bank.class, 23L);
+			
+			System.out.println("proxy first and upon invoke execute the query");
+			
+			System.out.println(bank1.getName());
+			
+			
+		} catch (Exception e) {
+			throw e;
+			
+		} finally {
+			session.close();
+		}
+	}
+	
+	
+	@Test(expected  = NullPointerException.class)
+	public void testGetApiError() {
+		Session session = sessionFactory.openSession();
+		
+		try {
+			
+			Bank bank = session.get(Bank.class, 13L);
+			System.out.println("executed query first");
+			
+			System.out.println(bank.getName());		
+			
+		} catch (Exception e) {
+			throw e;
+			
+		} finally {
+			session.close();
+		}
+	}
+	
+	@Test
+	public void testGetAndLoadApi() {
+		Session session = sessionFactory.openSession();
+		
+		try {
+			
+			Bank bank = session.get(Bank.class, 1L);
+			System.out.println("executed query first");
+			assertNotNull(bank);
+			System.out.println(bank.getName());
+			
+			Bank bank1 = session.load(Bank.class, 2L);
+			
+			System.out.println("proxy first and upon invoke execute the query");
+			
+			System.out.println(bank1.getName());
+			assertNotNull(bank1);
+			
+		} catch (Exception e) {
+			throw e;
+			
+		} finally {
+			session.close();
 		}
 	}
 	
@@ -440,4 +493,42 @@ public class AppTest
 		return account;	
 		
 	}
+	
+	@BeforeClass
+	public static void prepareDb() {
+		Session session = sessionFactory.openSession();
+		
+		try {
+			session.beginTransaction();
+			for(int i=0;i<5;i++) {
+				Bank bank  = createNewBank(i);
+				session.save(bank);
+				
+			}
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			throw new RuntimeException(e);
+		} finally {
+			session.close();
+		}
+	}
+	
+	private static Bank createNewBank(int i) {
+		Bank bank = new Bank();
+		bank.setCreatedBy("creator-"+i);
+		bank.setCreatedDate(new Date());
+		bank.setInternational(true);
+		bank.setLastUpdatedDate(new Date());
+		bank.setName("Bank-"+i);
+		bank.setUpdatedBy("updater-"+i);
+		return bank;
+	}
+
+	@AfterClass
+	public static void tearDownAll() {
+		sessionFactory.close();
+	}
 }
+
+
